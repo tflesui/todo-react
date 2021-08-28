7dc66bb311e8fc1f284fc02f1cdf9d3ac6131898
Francis Lesui <37851797+tflesui@users.noreply.github.com>
2021-08-27 20:07:26 -0700

    Added Delete button functionality

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -3,6 +3,20 @@ import { useState, useEffect } from 'react';
 const ListTodos = () => {
   const [todos, setTodos] = useState([]);
 
+  // Delete Todos Function
+  const deleteTodo = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: 'DELETE',
+      });
+
+      setTodos(todos.filter((todo) => todo.todo_id !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  // Get Todos Function
   const getTodos = async () => {
     const response = await fetch('http://localhost:5000/todos');
 
@@ -38,7 +52,14 @@ const ListTodos = () => {
             <tr key={todo.todo_id}>
               <td>{todo.description}</td>
               <td>Edit</td>
-              <td>Delete</td>
+              <td>
+                <button
+                  className='btn btn-danger'
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
