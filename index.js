116651421d8e263f2e67437e050b2c3ea6fb2c52
Francis Lesui <37851797+tflesui@users.noreply.github.com>
2021-08-29 18:40:30 -0700

    Moved more files for deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
-const client = require('./db.js');
+const client = require('./db');
 client.connect();
 
 // middleware
