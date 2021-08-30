c57fbbfe01d44e4f68a8555d416d99933cba953e
Francis Lesui <37851797+tflesui@users.noreply.github.com>
2021-08-29 20:26:19 -0700

    removed localhost from endpoints

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -7,7 +7,7 @@ const ListTodos = () => {
   // Delete Todos Function
   const deleteTodo = async (id) => {
     try {
-      const response = await fetch(`http://localhost:5000/todos/${id}`, {
+      const response = await fetch(`/todos/${id}`, {
         method: 'DELETE',
       });
 
@@ -19,7 +19,7 @@ const ListTodos = () => {
 
   // Get Todos Function
   const getTodos = async () => {
-    const response = await fetch('http://localhost:5000/todos');
+    const response = await fetch('/todos');
 
     const todosArray = await response.json();
 
