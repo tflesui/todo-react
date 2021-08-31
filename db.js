bb0ad0426c01096ff51512c91b5a2a9aa93584a5
Francis Lesui <37851797+tflesui@users.noreply.github.com>
2021-08-30 21:06:39 -0700

    Added connection string

--- db.js
+++ db.js
@@ -1,5 +1,12 @@
 const { Client } = require('pg');
 
-const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/todo_react');
+// const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/todo_react');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/todo_react',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 module.exports = client;
--- index.js
+++ index.js
@@ -14,9 +14,9 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 //ROUTES
 
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 //get all todos
